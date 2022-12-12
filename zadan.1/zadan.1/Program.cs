c7fdee5d4fbb2c6cababcfb5a39899fb25531f8d
nestyxi <120363600+nestyxi@users.noreply.github.com>
2022-12-12 10:48:57 +0300

    Add files via upload

---
+++ zadan.1/zadan.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба");
+            double x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Длина ребра = " + x.ToString());
+            Console.WriteLine("Объем = " + Math.Pow(x, 3).ToString());
+            Console.WriteLine("Площадь грани = " + Math.Pow(x, 2).ToString());
+            Console.WriteLine("Площадь поверхности = " + (Math.Pow(x, 2) * 6).ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
